dff72d984bf476be35eb21b733dfcd448efa5b06
HoangTuBangGia <caubetritue123@gmail.com>
2025-08-24 16:21:37 +0700

    leetcode 217

---
+++ practice/leetcode217.go
@@ -0,0 +1,13 @@
+func containsDuplicate(nums []int) bool {
+	count := make(map[int]bool)
+
+	for _, num := range nums {
+		if count[num] {
+			return true
+		}
+
+		count[num] = true
+	}
+
+	return false
+}
