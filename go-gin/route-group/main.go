02c92cc2aa80a9eb9284deb6f7055d6ba6cd93b3
HoangTuBangGia <caubetritue123@gmail.com>
2025-08-26 21:35:03 +0700

    them /

--- go-gin/route-group/main.go
+++ go-gin/route-group/main.go
@@ -10,26 +10,26 @@ import (
 func main() {
 	r := gin.Default()
 
-	v1 := r.Group("api/v1")
+	v1 := r.Group("/api/v1")
 	{
-		user := v1.Group("users")
+		user := v1.Group("/users")
 		{
 			userHandlerV1 := v1handler.NewUserHandler()
 
-			user.GET("", userHandlerV1.GetUsersV1)
-			user.GET(":id", userHandlerV1.GetUsersByIdV1)
-			user.POST("", userHandlerV1.PostUsersV1)
-			user.PUT(":id", userHandlerV1.PutUsersV1)
-			user.DELETE(":id", userHandlerV1.DeleteUsersV1)
+			user.GET("/", userHandlerV1.GetUsersV1)
+			user.GET("/:id", userHandlerV1.GetUsersByIdV1)
+			user.POST("/", userHandlerV1.PostUsersV1)
+			user.PUT("/:id", userHandlerV1.PutUsersV1)
+			user.DELETE("/:id", userHandlerV1.DeleteUsersV1)
 		}
 
 		product := v1.Group("/products")
 		{
 			productHandlerV1 := v1handler.NewProductHandler()
 
-			product.GET("", productHandlerV1.GetProductsV1)
+			product.GET("/", productHandlerV1.GetProductsV1)
 			product.GET("/:id", productHandlerV1.GetProductsByIdV1)
-			product.POST("", productHandlerV1.PostProductsV1)
+			product.POST("/", productHandlerV1.PostProductsV1)
 			product.PUT("/:id", productHandlerV1.PutProductsV1)
 			product.DELETE("/:id", productHandlerV1.DeleteProductsV1)
 		}
@@ -41,11 +41,11 @@ func main() {
 		{
 			userHandlerV2 := v2handler.NewUserHandler()
 
-			user.GET("", userHandlerV2.GetUsersV2)
-			user.GET(":id", userHandlerV2.GetUsersByIdV2)
-			user.POST("", userHandlerV2.PostUsersV2)
-			user.PUT(":id", userHandlerV2.PutUsersV2)
-			user.DELETE(":id", userHandlerV2.DeleteUsersV2)
+			user.GET("/", userHandlerV2.GetUsersV2)
+			user.GET("/:id", userHandlerV2.GetUsersByIdV2)
+			user.POST("/", userHandlerV2.PostUsersV2)
+			user.PUT("/:id", userHandlerV2.PutUsersV2)
+			user.DELETE("/:id", userHandlerV2.DeleteUsersV2)
 		}
 	}
 
